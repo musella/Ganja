08c37036d171ad168480acd6d33ee10c748e8d85
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2017-11-02 11:03:34 +0100

    writing to EOS

--- GanjaTree/test/sendOnBatch.py
+++ GanjaTree/test/sendOnBatch.py
@@ -18,6 +18,8 @@ inputlist = "files_"+dataset+".txt"
 
 queue = "8nh"
 
+pwd = os.environ['PWD']
+
 eosdir = "/eos/cms/store/user/pandolf/Ganja/" + prodName + "/" + dataset
 os.system("eos mkdir " + eosdir) 
 
@@ -29,26 +31,23 @@ os.system("mkdir -p "+dir+"/output/")
 os.system("mkdir -p "+dir+"/src/")
 
 
-#look for the current directory
-#######################################
-pwd = os.environ['PWD']
-#######################################
 inputListfile=open(inputlist)
 inputfiles = inputListfile.readlines()
 ijob=0
 
-#copy the configuration in the actual run directory
-#os.system("cp -r config "+dataset_name)
 
 while (len(inputfiles) > 0):
 
+    outfile = "ganjaTree_"+str(ijob)+".root"
+    
     cfgname = dir+"/input/ganjatree_cfg_" + str(ijob) + ".py"
     with open(pwd+"/ganjatree_cfg_TMPL.py", "rt") as fin:
        with open(cfgname, "wt") as fout:
           for line in fin:
             if 'XXXOUTFILE' in line:
+               fout.write(line.replace('XXXOUTFILE', outfile))
                #fout.write(line.replace('XXXOUTFILE', "root://eoscms.cern.ch//"+eosdir+"/ganjaTree_"+str(ijob)+".root"))
-               fout.write(line.replace('XXXOUTFILE', pwd+"/"+dir+"/output/ganjaTree_"+str(ijob)+".root"))
+               #fout.write(line.replace('XXXOUTFILE', pwd+"/"+dir+"/output/ganjaTree_"+str(ijob)+".root"))
             elif 'XXXFILES' in line:
                for ntp in range(0,min(ijobmax,len(inputfiles))):
                   ntpfile = inputfiles.pop()
@@ -64,7 +63,9 @@ while (len(inputfiles) > 0):
     outputfile.write('export SCRAM_ARCH=slc6_amd64_gcc472\n')
     outputfile.write('cd /afs/cern.ch/work/p/pandolf/CMSSW_5_3_32_Ganja/src/; eval `scramv1 runtime -sh` ; cd -\n')
     outputfile.write('cd $WORKDIR\n')
-    outputfile.write('cmsRun '+pwd+'/'+cfgname)
+    outputfile.write('cmsRun '+pwd+'/'+cfgname+'\n')
+    outputfile.write('cp '+outfile+' '+eosdir+'\n')
+    outputfile.write('rm '+outfile+'\n')
     #outputfile.write('ls '+analyzerType+'*.root | xargs -i scp -o BatchMode=yes -o StrictHostKeyChecking=no {} pccmsrm25:'+diskoutputmain+'/{}\n') 
     #outputfile.write('cp *.root '+diskoutputmain2+'\n') 
     outputfile.close
